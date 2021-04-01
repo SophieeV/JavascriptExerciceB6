7ab010ce315e30a02b0a9d05880e31fd76301215
SophieeV <sophievesin@wanadoo.fr>
2021-04-01 10:33:31 +0200

    ajout fichiers

---
+++ script.js
@@ -0,0 +1,35 @@
+//image1
+function passage1(element) {
+element.setAttribute('src', 'images/image1.jpg');
+}
+function departure1(element) {
+element.setAttribute('src', 'images/image1_2.jpg');
+}
+//image2
+function passage2(element) {
+element.setAttribute('src', 'images/image2.jpg');
+}
+function departure2(element) {
+element.setAttribute('src', 'images/image2_2.jpg');
+}
+//image 3
+function passage3(element) {
+element.setAttribute('src', 'images/image3.jpg');
+}
+function departure3(element) {
+element.setAttribute('src', 'images/image3_2.jpg');
+}
+//image4
+function passage4(element) {
+element.setAttribute('src', 'images/image4.jpg');
+}
+function departure4(element) {
+element.setAttribute('src', 'images/image4_2.jpg');
+}
+//image 5
+function passage5(element) {
+element.setAttribute('src', 'images/image5.jpg');
+}
+function departure5(element) {
+element.setAttribute('src', 'images/image5_2.jpg');
+}
